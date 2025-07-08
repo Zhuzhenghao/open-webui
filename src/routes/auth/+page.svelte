bf9686c5ae9f985b69a2fac025f4fbaa921031fa
zhenghao.zhu <zhenghao.zhu@daocloud.io>
2025-07-08 17:19:26 +0800

    add id login-with-oidc

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -437,6 +437,7 @@
 								{/if}
 								{#if $config?.oauth?.providers?.oidc}
 									<button
+                    id="login-with-oidc"
 										class="flex justify-center items-center bg-gray-700/5 hover:bg-gray-700/10 dark:bg-gray-100/5 dark:hover:bg-gray-100/10 dark:text-gray-300 dark:hover:text-white transition w-full rounded-full font-medium text-sm py-2.5"
 										on:click={() => {
 											window.location.href = `${WEBUI_BASE_URL}/oauth/oidc/login`;
